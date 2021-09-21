bef6bc9332f7f0d991e5c346524ec08820fc01f0
nermint <talibovanermin1999@gmail.com>
2021-09-21 18:28:00 +0400

    directive created

--- src/app/shared/directives/separated.directive.ts
+++ src/app/shared/directives/separated.directive.ts
@@ -4,75 +4,19 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[appSeparated]'
 })
 export class SeparatedDirective {
-  DECIMAL_SEPARATOR=".";
-  GROUP_SEPARATOR=",";
-  // private regex: RegExp = new RegExp(/^\d*\,?\d{0,3}$/g);
-  // // Allow key codes for special events. Reflect :
-  // // Backspace, tab, end, home
-  // private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Del', 'Delete'];
-
-  constructor(private el: ElementRef) {
-  }
+  private regex: RegExp = new RegExp(/^\d*\,?\d{0,3}$/g);
+  private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Del', 'Delete'];
+  constructor(private el: ElementRef) { }
   @HostListener('keydown', ['$event'])
-
-  // onKeyDown(event: KeyboardEvent) {
-  //   console.log(this.el.nativeElement.value);
-  //   if (this.specialKeys.indexOf(event.key) !== -1) {
-  //     return;
-  //   }
-  //   let current: string = this.el.nativeElement.value;
-  //   const position = this.el.nativeElement.selectionStart;
-  //   const next: string = [current.slice(0, position), event.key === 'Decimal' ? ',' : event.key, current.slice(position)].join('');
-  //   if (next && !String(next).match(this.regex)) {
-  //     event.preventDefault();
-  //   }
-
-  // format(valString) {
-  //   if (!valString) {
-  //       return '';
-  //   }
-  //   let val = valString.toString();
-  //   const parts = this.unFormat(val).split(this.DECIMAL_SEPARATOR);
-  //   return parts[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, this.GROUP_SEPARATOR)
-
-  // };
-
-  // unFormat(val) {
-  //     if (!val) {
-  //         return '';
-  //     }
-  //     val = val.replace(/^0+/, '').replace(/\D/g,'');
-  //     if (this.GROUP_SEPARATOR === ',') {
-  //         return val.replace(/,/g, '');
-  //     } else {
-  //         return val.replace(/\./g, '');
-  //     }
-  // };
-
-
-
-  // }
-   onKeyDown(event: KeyboardEvent) {
-    if (!this.el.nativeElement.value) {
-        return '';
+  onKeyDown(event: KeyboardEvent) {
+    if (this.specialKeys.indexOf(event.key) !== -1) {
+      return;
     }
-    let val = this.el.nativeElement.value.toString();
-    const parts = this.unFormat(val).split(this.DECIMAL_SEPARATOR);
-    return parts[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, this.GROUP_SEPARATOR)
-   }
+    const current: string = this.el.nativeElement.value;
+    const next: string = current.concat(event.key);
+    if (next && !String(next).match(this.regex)) {
+      event.preventDefault();
 
-    unFormat(val) {
-      if (!val) {
-          return '';
-      }
-      val = val.replace(/^0+/, '').replace(/\D/g,'');
-      if (this.GROUP_SEPARATOR === ',') {
-          return val.replace(/,/g, '');
-      } else {
-          return val.replace(/\./g, '');
-      }
-    };
-
-
-
+    }
+  }
 }
